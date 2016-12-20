c15b8f6f58b0ea9c658b88c5240fd4617c7979c7
Ethan Suttner <ethan.suttner@gmail.com>
2016-12-19 21:15:45 -0500

    add add

--- foo.js
+++ foo.js
@@ -1,4 +1,8 @@
 console.log(document.getElementsByTagName("body")[0]);
 document.getElementsByTagName("body")[0].style.backgroundColor = "black";
-console.log(document.getElementsByTagName("body")[0]);
-// .style.color = "blue";
+
+console.log(add(2,4));
+
+function add(x,y) {
+  return x+y;
+}
