69693b9b3555140247473f835e026e461046eec6
Ethan Suttner <ethan.suttner@gmail.com>
2016-12-19 21:07:20 -0500

    test

--- foo.js
+++ foo.js
@@ -1,4 +1,4 @@
-console.log(document.getElementsByTagName("body"));
-document.getElementsByTagName("body").style.backgroundColor = "black";
-console.log(document.getElementsByTagName("body"));
+console.log(document.getElementsByTagName("body")[0]);
+document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+console.log(document.getElementsByTagName("body")[0]);
 // .style.color = "blue";
