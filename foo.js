3e81db519d4822801011e351a8ec5b71e2f204a4
Ethan Suttner <ethan.suttner@gmail.com>
2016-12-26 16:52:19 -0500

    debug

--- foo.js
+++ foo.js
@@ -6,7 +6,7 @@ function myFunction() {
   var i;
   var colors = ["#EA4335", "#4285F4", "#34A853", "#FBBC05"];
   for (i = 0; i < myNodelist.length; i++) {
-    myNodelist[i].style.backgroundColor = colors[getRandomInt(0,colors.length)];
+    // myNodelist[i].style.backgroundColor = colors[getRandomInt(0,colors.length)];
     // console.log(myNodelist[i].innerText);
     myNodelist[i].text = "Foo";
   }
